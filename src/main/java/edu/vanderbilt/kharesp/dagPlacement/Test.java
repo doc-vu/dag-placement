ea9e98c7e5e8851c51f1950d2f8cf5e307b0722f
kharesp <shweta.p.khare@gmail.com>
2018-11-06 10:11:17 -0600

    moved latency plot to correct directory

--- src/main/java/edu/vanderbilt/kharesp/dagPlacement/Test.java
+++ src/main/java/edu/vanderbilt/kharesp/dagPlacement/Test.java
@@ -1,10 +1,27 @@
 package edu.vanderbilt.kharesp.dagPlacement;
 
-import java.nio.file.Paths;
 
 public class Test {
 	public static void main(String args[]){
-		System.out.println(Paths.get(".").toAbsolutePath().normalize().toString());
+		if (args.length<1){
+			System.out.println("Usage: Test t");
+			return;
+		}
+		int t=Integer.parseInt(args[0]);
+		for(int i=0;i<10000;i++){
+			long startTs = System.currentTimeMillis();
+			for(int j=0;j<t;j++){
+				fib(22);
+			}
+			long endTs = System.currentTimeMillis();
+			System.out.format("%d,%d,%d\n",i+1, t, (endTs - startTs));
+		}
+	}
+
+	public static int fib(int n) {
+		if (n <= 1)
+			return n;
+		return fib(n - 1) + fib(n - 2);
 	}
 
 }
